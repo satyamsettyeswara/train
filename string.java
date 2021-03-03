5120753b5e1f6e531baf72adc57e2b8f8d39eca7
Brahmaji <satyamsetty.eswara@gmail.com>
2021-03-03 14:31:40 +0530

    added function to print character by character

--- string.java
+++ string.java
@@ -3,7 +3,12 @@ public class string{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		System.out.println(s);
+		print(s);
+	}
+	static void print(String s){
+		for(int i=0;i<S.length();i++){
+			System.out.print(s.charAt(i)+" ");
+		}
 	}
 }
 
