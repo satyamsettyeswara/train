ade5a7a5744c3eabcddc367993e5512a0d837dc9
Brahmaji <satyamsetty.eswara@gmail.com>
2021-03-03 14:26:28 +0530

    updated the string variable acess from the uses

--- string.java
+++ string.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class string{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
+		String s = sc.next();
 		System.out.println(s);
 	}
 }
