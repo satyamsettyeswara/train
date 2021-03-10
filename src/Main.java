61697cac10a241e83426db92a2377b3971178a99
Brahmaji <31738585+satyamsettyeswara@users.noreply.github.com>
2021-03-10 11:39:27 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
+		String s = "hello";
 		print(s);	
 		reverse(s);
 	}
