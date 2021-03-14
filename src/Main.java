0568370bbf45209bd1cf59a7a0b54b40115dad48
Brahmaji <satyamsetty.eswara@gmail.com>
2021-03-14 14:48:39 +0530

    Added new caps method to the project

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ public class Main{
 		Scanner sc = new Scanner(System.in);
 		String s = "Pirate";
 		print(s);
+		caps(s);
 	}
 	static void print(String s){
 		for(int i=0;i<s.length();i++){
 			System.out.print(s.charAt(i)+" - ");
 		}
+	}
+	static void caps(String s){
+		System.out.println(s.toString());
 	}	
 }
 
