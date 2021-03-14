c4114f12968b6076ca53d8d21aed538a7ccfa8a1
Brahmaji <satyamsetty.eswara@gmail.com>
2021-03-14 11:55:20 +0530

    Removed calling method for reverse

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		String s = "hello";
-		print(s);	
-		reverse(s);
+		print(s);			
 	}
 	static void print(String s){
 		for(int i=0;i<s.length();i++){
